9cfc0b4fdd75ca3918b9ce2d35e92ce4f37a08d8
unknown <michellem123tuscano@gmail.com>
2024-07-07 08:31:10 +0800

    first commit

---
+++ calculator/js/js.js
@@ -0,0 +1,17 @@
+function clearInput() {
+    document.getElementById('display').value = '';
+  }
+  
+  function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+  }
+  
+  function calculate() {
+    try {
+      const result = eval(document.getElementById('display').value);
+      document.getElementById('display').value = result;
+    } catch (error) {
+      document.getElementById('display').value = 'Error';
+    }
+  }
+  
